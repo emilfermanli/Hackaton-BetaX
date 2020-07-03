ebf9e9f371fef9c4e500cc54fc79f44cba37acd7
emil fermanli <emilfermanli@gmail.com>
2020-07-03 21:29:24 +0400

    Hackaton

---
+++ buildHC/precache-manifest.95d9694ab4f19ebd296871c57952d130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b3e83a7c7ff5643ab83cf7417bda35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef9cf83adf4d4915ac3",
+    "url": "/static/css/2.e8e2b19f.chunk.css"
+  },
+  {
+    "revision": "829dc087e00711acc629",
+    "url": "/static/css/main.b6505bba.chunk.css"
+  },
+  {
+    "revision": "1ef9cf83adf4d4915ac3",
+    "url": "/static/js/2.5c8dca3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c8dca3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "829dc087e00711acc629",
+    "url": "/static/js/main.d5c7e276.chunk.js"
+  },
+  {
+    "revision": "df1fcec34d08ca1f6427",
+    "url": "/static/js/runtime-main.dfcad795.js"
+  }
+]);
