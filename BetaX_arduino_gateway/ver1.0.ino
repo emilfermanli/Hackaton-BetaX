27b76248dacebfaba1f7b77295df6b11bd41e512
Eldaronics <46577464+Eldaronics@users.noreply.github.com>
2020-07-04 22:59:28 +0400

    qoshulma ile bagli problemler aradan qalxdi
    serverə 2 random rəqəm göndərilir.Test məqsədli

--- BetaX_arduino_gateway/ver1.0.ino
+++ BetaX_arduino_gateway/ver1.0.ino
@@ -1,93 +1,85 @@
-/Gateway test code/
 #include <Arduino.h>
 #include <SoftwareSerial.h>
-#include <String.h>
-SoftwareSerial gprsSerial(2,3); //RX = 2, TX = 3
+SoftwareSerial myGsm(7,8);
+float temp;
+//debug uchun
+int int1 = 25;
+int int2 = 19;
+void printSerialData()
+{
+ while(myGsm.available()!=0)
+ Serial.write(myGsm.read());
+}
+
+void sendData()
+{
+String dataPacket = (String(int1) + "," + String(int2) + "*");
+ myGsm.print("*");
+ myGsm.println();
+ delay(5000);
+ printSerialData();
+ myGsm.write(0x1A);
+ delay(5000);
+ printSerialData();
+}
 
 void setup()
 {
-  gprsSerial.begin(9600);               // set the gsm module baud rate   
-  Serial.begin(9600);   
-  delay(1000);
+ myGsm.begin(9600);  
+ Serial.begin(9600);  
+ delay(500);
+int1++;
+int2++;
+if(int1 >255){
+int1 = 0;
+int2 = 0;
 }
- void ShowSerialData()
-{
-  while(gprsSerial.available()!=0)
-  Serial.write(gprsSerial.read());
-  delay(5000); 
+myGsm.println("AT+CIPSHUT");
+ delay(1000);
+ printSerialData();
+
+ myGsm.println("AT+CIPMUX=0");
+ delay(2000);
+ printSerialData();
+ 
+ myGsm.println("AT+CGATT=1");
+ delay(1000);
+ printSerialData();
+ 
   
+ myGsm.println("AT+CSTT=\"nar\",\"\",\"\"");//setting the APN,2nd parameter empty works for all networks 
+ delay(5000);
+ printSerialData();
+ 
+ myGsm.println();
+ myGsm.println("AT+CIICR");
+ delay(6000);
+ printSerialData();
+ 
+ myGsm.println("AT+CIFSR"); //init the HTTP request
+ delay(2000); 
+ printSerialData();
+ 
+ myGsm.println("AT+CIPSTART=\"TCP\",\"64.227.107.166\",\"8080\"");
+ delay(5000);
+ printSerialData();
+ delay(5000);
+ 
+ myGsm.println("AT+CIPSEND");
+ delay(2000);
+ printSerialData();
+
+sendData();
+delay(3000);
+myGsm.println("AT+CIPCLOSE");
+printSerialData();
+
+myGsm.println("AT+CIPSHUT");
+delay(1000);
+printSerialData();
 }
+
 void loop()
 {
-  if (gprsSerial.available())
-    Serial.write(gprsSerial.read());
- //GSM initial configure 
-  gprsSerial.println("AT");
-  delay(1000);
- 
-  gprsSerial.println("AT+CPIN?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CREG?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CGATT?");
-  delay(1000);
- 
-  gprsSerial.println("AT+CIPSHUT");
-  delay(1000);
- 
-  gprsSerial.println("AT+CIPSTATUS");
-  delay(2000);
- 
-  gprsSerial.println("AT+CIPMUX=0");
-  delay(2000);
- 
-  ShowSerialData();
- 
-  gprsSerial.println("AT+CSTT=\"nar\"");//start task and setting the APN
-  delay(1000);
- 
-  ShowSerialData();
- 
-  gprsSerial.println("AT+CIICR");//bring up wireless connection
-  delay(3000);
- 
-  ShowSerialData();
- 
-  gprsSerial.println("AT+CIFSR");//get local IP adress
-  delay(2000);
- 
-  ShowSerialData();
- 
-  gprsSerial.println("AT+CIPSPRT=0");
-  delay(3000);
-
-  ShowSerialData();
-    //GSM initial configure end
-  gprsSerial.println("AT+CIPSTART=\"TCP\",\"http://64.227.107.166/ \",\"80\"");//connect to server http://64.227.107.166/
-  delay(6000);
- 
-  ShowSerialData();
- 
-  gprsSerial.println("AT+CIPSEND");//begin send data to remote server
-  delay(4000);
-  ShowSerialData();
-  
-  String str="$36,5987,56565,545242,567,897,11,23,26*"; //test data
-  Serial.println(str);
-  gprsSerial.println(str);//begin send data to remote server
-  
-  delay(4000);
-  ShowSerialData();
- 
-  gprsSerial.println((char)26);//sending
-  delay(5000);//waitting for reply, important! the time is base on the condition of internet 
-  gprsSerial.println();
- 
-  ShowSerialData();
- 
-  gprsSerial.println("AT+CIPSHUT");//close the connection
-  delay(100);
-  ShowSerialData();
+sendData();
 }
